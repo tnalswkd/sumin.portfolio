6837da3ad204f649109ee3b18307171c5cd56f25
tnalswkd <tnalswkd1122@naver.com>
2025-04-29 11:09:16 +0900

    feat(theme): add theming support and implement layout components
    - Introduced Header and Footer components
    - Updated global styles for dark/light themes
    - Integrated local font

---
+++ src/app/_components/Header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <h1 className="text-red-500">Header</h1>
+    </header>
+  );
+};
+
+export default Header;
